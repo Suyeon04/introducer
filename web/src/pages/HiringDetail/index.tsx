b96c826b4153ddf3c016a5f9c8d5c699205b69a5
Suyeon <s2026@e-mirim.hs.kr>
2024-04-24 22:11:01 +0900

    style : 슬라이드 위젯 css 변경

--- web/src/pages/HiringDetail/index.tsx
+++ web/src/pages/HiringDetail/index.tsx
@@ -5,16 +5,26 @@ const data = {
   headerImageUrlArray: [
     "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI",
     "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI",
+    "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI",
+    "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI",
   ],
 };
 
 const Container = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
 `;
 const Header = styled.div`
   width: 100%;
-  height: 100%;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+`;
+const Body = styled.div`
+  display: flex;
+  width: 100%;
 `;
 
 export default function LoginPage() {
@@ -22,12 +32,14 @@ export default function LoginPage() {
     <>
       <Container>
         <Header>
-          <div>
-            <SlideImage imageUrlArray={data.headerImageUrlArray} />
-          </div>
+          <SlideImage imageUrlArray={data.headerImageUrlArray} />
         </Header>
-        <div>asdf</div>
-        <div>asdf</div>
+        <Body>
+          <div>
+            <div>asdf</div>
+            <div>asdf</div>
+          </div>
+        </Body>
       </Container>
     </>
   );
