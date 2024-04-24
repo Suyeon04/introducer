e43ce7b355093e6b1b833ee190c95f8e91c28670
Suyeon <s2026@e-mirim.hs.kr>
2024-04-24 22:12:07 +0900

    repactor : props type, interface 선언으로 변경, map 가독성 개선

--- web/src/widgets/SlideImage/index.tsx
+++ web/src/widgets/SlideImage/index.tsx
@@ -1,35 +1,35 @@
-import Image from '@/entities/ui/image';
-import { css } from '@emotion/react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { Pagination, Navigation } from 'swiper/modules';
+import Image from "@/entities/ui/image";
+import { css } from "@emotion/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper/modules";
 
-type Props = {
-    imageUrlArray: string[];
-};
+interface Props {
+  imageUrlArray: string[];
+}
 
 export default function SlideImage({ imageUrlArray }: Props) {
-    return (
-        <Swiper
-            spaceBetween={10}
-            pagination={{
-                type: 'progressbar',
-            }}
-            navigation={true}
-            modules={[Pagination, Navigation]}
-            className="mySwiper"
-            breakpoints={{
-                976: {
-                    slidesPerView: 2,
-                },
-            }}
-        >
-            {imageUrlArray.map(function (imageUrl, index) {
-                return <SwiperSlide key={index}><Image src={imageUrl} /></SwiperSlide>
-            })}
-
-        </Swiper>
-    );
+  return (
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        type: "progressbar",
+      }}
+      navigation={true}
+      modules={[Pagination, Navigation]}
+      breakpoints={{
+        976: {
+          slidesPerView: 2,
+        },
+      }}
+    >
+      {imageUrlArray.map((imageUrl, index) => (
+        <SwiperSlide key={index}>
+          <Image src={imageUrl} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
